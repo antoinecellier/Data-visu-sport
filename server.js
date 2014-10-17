8fc87c9a12ad2e8c78a2ea8e128c480cf73347bf
macilry <antoine08.cellier@hotmail.fr>
2014-10-17 20:49:59 +0200

    change server status

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+var port_number = server.listen(process.env.PORT || 3000);
 
 app.use('/', express.static(__dirname + '/'));
 
-module.exports = app.listen(8080, function() {
+module.exports = app.listen(port_number, function() {
 	console.log("Express server listening on port " + app.get('port'));
 });
 
