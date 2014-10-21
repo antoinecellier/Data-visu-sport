66c7e159f237bf8b00f014f21a592e796f07967d
Alex Sampaio <alex.sampaio1991@gmail.com>
2014-10-21 02:30:38 +0200

    page departement details

--- assets/js/directives/lineChartDirective.js
+++ assets/js/directives/lineChartDirective.js
@@ -10,8 +10,8 @@ DataVizSport.directive('linechart', ['DatasService','$http','$location',
 				// console.log(datas);
 
 				// Mise en variable des données
-				var width = 500,
-					height = 500,
+				var width = 380,
+					height = 380,
 					radius = Math.min(width, height) / 2;
 					radiusImage = Math.min(width, height) / 2;
 
@@ -24,11 +24,11 @@ DataVizSport.directive('linechart', ['DatasService','$http','$location',
 
 				// retourner l'arc
 				var arc = d3.svg.arc()
-					.innerRadius(radius - 150)
+					.innerRadius(radius - 80)
 					.outerRadius(radius);
 
 				var arcImage = d3.svg.arc()
-					.innerRadius(radiusImage - 150)
+					.innerRadius(radiusImage - 80)
 					.outerRadius(radiusImage);
 
 				// var arcBorder = d3.svg.arc()
@@ -43,13 +43,31 @@ DataVizSport.directive('linechart', ['DatasService','$http','$location',
 					.append("g")
 					.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-				// création d'un objet pour matcher aux données attendues
+				/**
+				 * Génération du rectangle pour mettre les données
+				 */
+				var restData = svg.append("g")
+				.attr("class", "rectZone");
+
+				restData.append("rect")
+				.attr("transform", function(d) { return "translate(-" + width/3 + ", -" + height/3 +")"; })
+				.attr("fill", "white")
+				.attr("width", width/1.5 + "px")
+				.attr("height", height/1.5 + "px")
+				.attr("class", function(d) { return "rectData"; });
+
+				/**
+				 * Création d'un objet pour matcher aux données attendues
+				 */
 				var var_graph = [];
 				angular.forEach(datas.sport, function(value, key){
 					temp = { "sport": 100, "classe": key}
 					var_graph.push(temp);
 				});
 
+				/**
+				 * Mise en place du graph
+				 */
 				var path = svg.datum(var_graph).selectAll("path")
 					.data(pie)
 					.enter().append("g")
@@ -67,19 +85,9 @@ DataVizSport.directive('linechart', ['DatasService','$http','$location',
 
 					//survol
 					.on('mouseover', function(d){
-						//modifie les arcs
-						path.selectAll("." + d.data.classe).attr("fill", "white");
-						path.selectAll("." + d.data.classe).style("stroke", "black").style('stroke-width', 0.5);
-
-						//modifie les images
-						path.selectAll(".text-" + d.data.classe).attr("xlink:href", (function(d) { return 'assets/sass/img/icon/' + d.data.classe + '.png'; }));
+						showSurvol(datas, d);
 					}).on('mouseout', function(d){
-						//modifie les arcs
-						path.selectAll("." + d.data.classe).attr("fill", "black");
-						path.selectAll("." + d.data.classe).style("stroke", "white");
-
-						//modifie les images
-						path.selectAll(".text-" + d.data.classe).attr("xlink:href", (function(d) { return 'assets/sass/img/icon/w-' + d.data.classe + '.png'; }));
+						hideSurvol(datas, d);
 					});
 
 				// texte
@@ -102,20 +110,69 @@ DataVizSport.directive('linechart', ['DatasService','$http','$location',
 
 					//survol
 					.on('mouseover', function(d){
+						showSurvol(datas, d);
+					}).on('mouseout', function(d){
+						hideSurvol(datas, d);
+					});
+
+					showSurvol = function(dataOb, d){
 						//modifie les arcs
-						path.selectAll("." + d.data.classe).attr("fill", "white");
-						path.selectAll("." + d.data.classe).style("stroke", "black").style('stroke-width', 0.5);
+						path.selectAll("." + d.data.classe)
+							.attr("fill", "white")
+							.style("stroke", "black")
+							.style('stroke-width', 0.5);
 
 						//modifie les images
 						path.selectAll(".text-" + d.data.classe).attr("xlink:href", (function(d) { return 'assets/sass/img/icon/' + d.data.classe + '.png'; }));
-					}).on('mouseout', function(d){
+
+						//modifie le rectangle de tata
+						restData.selectAll(".rectData").attr("fill", "#babdc0");
+
+						var textrestData = restData.append("text")
+							.attr("class", "rectText")
+							.style("text-anchor", "middle")
+							.style("color", "black")
+							.style("font-size", "22px");
+
+						textrestData.
+							append("tspan")
+							.attr('x', 0)
+  							.attr('dy', -35)
+							.text("Licenciés : " + datas.sport[d.data.classe].nbLicence);
+
+						textrestData
+							.append("tspan")
+							.attr('x', 0)
+  							.attr('dy', 25)
+							.text("Équipements : " + datas.sport[d.data.classe].nbEquipement );
+
+						textrestData
+							.append("tspan")
+							.attr('x', 0)
+  							.attr('dy', 40)
+							.text(parseFloat(parseFloat(datas.sport[d.data.classe].nbEquipement)/datas.sport[d.data.classe].nbLicence).toFixed(2) + " par personne" );
+
+						textrestData
+							.append("tspan")
+							.attr('x', 0)
+  							.attr('dy', 50)
+  							.style("font-size", "13px")
+							.text("x 10 000");
+					};
+
+					hideSurvol = function(dataOb, d){
 						//modifie les arcs
-						path.selectAll("." + d.data.classe).attr("fill", "black");
-						path.selectAll("." + d.data.classe).style("stroke", "white");
+						path.selectAll("." + d.data.classe)
+							.attr("fill", "black")
+							.style("stroke", "white");
 
 						//modifie les images
 						path.selectAll(".text-" + d.data.classe).attr("xlink:href", (function(d) { return 'assets/sass/img/icon/w-' + d.data.classe + '.png'; }));
-					});
+
+						//modifie le rectangle de tata
+						restData.selectAll(".rectData").attr("fill", "white");
+						restData.selectAll(".rectText").remove();
+					}
 			}
 
 		}
